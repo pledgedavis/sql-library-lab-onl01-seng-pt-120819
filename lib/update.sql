09cce4c88a8fd8b715e33667a2310927c8bb651a
pledgedavis <pledgedavis14@gmail.com>
2020-02-28 16:28:20 -0500

    Done.

--- lib/update.sql
+++ lib/update.sql
@@ -1,3 +1,4 @@
 -- UPDATE table_name SET column1 = value1, column2 = value2,... 
 -- WHERE condition;
-UPDATE characters SET species = "Martian" WHERE id = 8; 
+-- UPDATE characters SET species
+UPDATE characters SET species = "Martian" WHERE id = (SELECT MAX(id) FROM characters)
